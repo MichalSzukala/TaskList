9c1cf47772bf5ee5964e97ae15447e946122063d
Michal Szukala <szukalagithub@gmail.com>
2018-03-28 20:04:15 +0200

    Add generic interface for list of tasks #10

---
+++ src/com/git/michalszukala/model/ListOfTasksInterface.java
@@ -0,0 +1,20 @@
+package com.git.michalszukala.model;
+
+import java.util.Date;
+
+/**
+ * Generic interface for the list of tasks
+ * @author Michal Szukala
+ */
+public interface ListOfTasksInterface <T>{
+    
+   public void addTaskToList(T task);
+   public void removeTask(int index);
+   public void markTaskAsDone(int index);
+   public void editTask(int index, String userEditMessage);
+   public void editStatus(int index, String userEditMessage);
+   public void editDueDate(int index, Date userEditMessage);
+   public void editProject(int index, String userEditMessage);
+   
+    
+}
